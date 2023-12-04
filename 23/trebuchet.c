8de5e14b5ce68df463640ef902bfcfae43c4bea4
Isaiah Paget <113209222+IsaiahPaget@users.noreply.github.com>
2023-12-03 18:28:44 -0800

    day 0 part 2

--- 23/trebuchet.c
+++ 23/trebuchet.c
@@ -1,27 +1,49 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+const char* words[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+const int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int isWord(char* string, size_t len, size_t i) {
+	char temp[50] = "\0"; 
+	for (size_t j = 0; j < len - i; j++) {
+		strcpy(temp, string);
+		temp[j] = '\0';
+		for (size_t k = 0; k < 9; k++) {
+			if (strcmp(temp, words[k]) == 0) {
+				return numbers[k];
+			}
+		}
+	}
+	return -1;
+}
 
 void solve(char* line, size_t len, char* answer) {
 	char current;
 	char first = '\0';
 	char last = '\0';
 	for (size_t i = 0; i < len - 1; i++) {
+		int val = isWord(line + i, len, i);
+		if (val != -1) {
+			if (first == '\0') {
+				first = val + '0';
+			}			
+			last = val + '0';
+
+		}
 		current = line[i];
 		if (isdigit(current) != 0) {
-			printf("%c isDigit\n", line[i]);
 			if (first == '\0') {
 				first = current;
 			}			
 			last = current;
 		}
 	}
-	printf("first: %c\n", first);
-	printf("last: %c\n", last);
 	answer[0] = first;
 	answer[1] = last;
 }
-
 int main() {
 	FILE* pFile;
 	char* pLine = NULL;
@@ -37,7 +59,6 @@ int main() {
 
 	while ((read = getline(&pLine, &len, pFile)) != -1) {
 		char answer[2];
-		printf("%s", pLine);
 		solve(pLine, read, answer);
 		finalNumber += atoi(answer);
 	}
